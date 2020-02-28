b1688c12232d54e9bb9f85a41545cc15e1bcddc7
Hannali Patidar <hannalip@gmail.com>
2020-02-27 23:03:11 -0500

    added config folder with connection and orm js files.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Meeses');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden', true);
+INSERT INTO burgers (burger_name) VALUES ('Owens');
