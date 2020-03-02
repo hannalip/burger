3d80ebe63cdc1bbcdb0275e40642d6c7ec094e04
Hannali Patidar <hannalip@gmail.com>
2020-03-02 06:06:22 -0500

    added some css.

--- models/burger.js
+++ models/burger.js
@@ -2,26 +2,20 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
+  selectAll: function (cb) {
+      orm.selectAll("burgers", function (res) {
+          cb(res);
+      });
   },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
+  insertOne: function (vals, cb) {
+      orm.insertOne("burgers", "burger_name", vals, function (res) {
+          cb(res);
+      });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
+  updateOne: function (colNewVal, condition, cb) {
+      orm.updateOne("burgers", colNewVal, condition, function (res) {
+          cb(res);
+      });
   }
 };
 
